8c250baf83a78c1f13b3a8231ea218d1512a6219
Pedro Correa <pedro8correa@gmail.com>
2021-03-17 21:09:28 -0300

    refactor: removed ts files for deno

--- deps.ts
+++
@@ -1,5 +0,0 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { parse } from "https://deno.land/std/flags/mod.ts";
-
-export { Application, config, Router, parse };
